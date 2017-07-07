b5427df1ddb23cba83e26cbffa1eff4ad534ed7c
Angelo <angeloangeles.nz@gmail.com>
2017-07-07 15:54:08 +1200

    changed name of bundle

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.set('view engine', 'hbs');
 app.set('port', (process.env.PORT || 3000));
 
 app.get('/', function (req, res) {
-  res.render('index', {name: 'index'});
+  res.render('index', {name: 'ic2'});
 });
 
 app.get('/:dashboard', function(req, res) {
